e7bccf5a5922f1156b4cb75770e28d10a942dc36
qu4ks <egorlapatskij@gmail.com>
2023-03-21 17:08:47 +0300

    Initial commit

---
+++ src/main/java/by/quaks/chatmanage/ChatManager.java
@@ -0,0 +1,17 @@
+package by.quaks.chatmanage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
