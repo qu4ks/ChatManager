e98fa283cb5dc42866a1d6a8ffb7f46cb014c7bc
qu4ks <egorlapatskij@gmail.com>
2023-04-09 00:09:42 +0300

    Event registration via EventRegisterer.java - ChatManager.java

--- src/main/java/by/quaks/chatmanager/ChatManager.java
+++ src/main/java/by/quaks/chatmanager/ChatManager.java
@@ -1,13 +1,16 @@
 package by.quaks.chatmanager;
 
+import by.quaks.chatmanager.listeners.BaseChatHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ChatManager extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        EventRegisterer.registerListeners(new Listener[]{
+                new BaseChatHandler()
+        },this);
     }
 
     @Override
