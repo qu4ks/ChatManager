0ac4926e7a2e06c5d4fdae4688205b0451ae19e8
qu4ks <egorlapatskij@gmail.com>
2023-04-09 00:07:28 +0300

    Utility event registration class - EventRegisterer.java

---
+++ src/main/java/by/quaks/chatmanager/EventRegisterer.java
@@ -0,0 +1,13 @@
+package by.quaks.chatmanager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+
+public class EventRegisterer {
+    public static void registerListeners(Listener[] a, Plugin plugin){ // Метод регистрации листенеров
+        for (Listener elem : a) {
+            Bukkit.getServer().getPluginManager().registerEvents(elem, plugin);
+        }
+    }
+}
