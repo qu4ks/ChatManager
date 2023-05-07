00a223618dc0a1e3c609c98740deb67805583014
qu4ks <egorlapatskij@gmail.com>
2023-05-07 14:20:00 +0300

    Proximity and global chat now exist

--- src/main/java/by/quaks/chatmanager/listeners/BaseChatHandler.java
+++ src/main/java/by/quaks/chatmanager/listeners/BaseChatHandler.java
@@ -1,20 +1,33 @@
 package by.quaks.chatmanager.listeners;
 
+import by.quaks.chatmanager.files.MainConfig;
 import by.quaks.chatmanager.utils.Message;
-import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-import java.lang.reflect.InvocationTargetException;
 
 public class BaseChatHandler implements Listener {
     @EventHandler
-    public void onPlayerChat(AsyncPlayerChatEvent event) throws InvocationTargetException {
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
         event.setCancelled(true);
         Player p = event.getPlayer();
         Message msg = new Message(event.getMessage(),event.getPlayer().getName());
-        p.spigot().sendMessage(msg.TextComponent());
+        switch (msg.getType()){
+            case GLOBAL:{
+                Bukkit.getServer().spigot().broadcast(msg.TextComponent());
+                break;
+            }
+            case LOCAL:{
+                for (Player player : Bukkit.getOnlinePlayers()){
+                    if (player.getLocation().distance(p.getLocation()) <= MainConfig.get().getInt("GlobalChatRadius")){
+                        player.spigot().sendMessage(msg.TextComponent());
+                    }
+                }
+                break;
+            }
+        }
     }
 }
